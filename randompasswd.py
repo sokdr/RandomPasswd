84b7038623d6071e22f9bad3608c8148fce2c928
sokdr <20625004+sokdr@users.noreply.github.com>
2026-02-11 14:04:37 +0200

    Update randompasswd.py
    changed script, added function password_generator

--- randompasswd.py
+++ randompasswd.py
@@ -4,14 +4,22 @@ import string
 print("*" * 50) 
 print("Welcome to the password generator\n ")
 print("*" * 50)
-pass_lenght = int(input("Please enter the password length: Please choose betwwen 12 and 20: "))
 
-if pass_lenght < 12 or pass_lenght > 20:
-    print("Please choose between 12 and 20")
-else:
-    print("*" * 50)
-    password = "".join(secrets.choice(string.ascii_letters + string.digits + string.punctuation) for i in range(pass_lenght))
-    password1 = "".join(secrets.choice(string.ascii_letters + string.digits + string.punctuation) for i in range(pass_lenght))
-    password2 = "".join(secrets.choice(string.ascii_letters + string.digits + string.punctuation) for i in range(pass_lenght))
-    print(f"1): {password}\n2): {password1}\n3): {password2}")
-    print("*" * 50)          
+
+
+def password_generator():
+
+    while True:
+        pass_length = int(input("Please enter the password length: Please choose between 12 and 20 characters length: "))
+        if pass_length < 12 or pass_length > 20:
+            print(f"Enter between 12 and 20 characters. ")
+            continue
+
+        for passwd in range(3):
+            password = "".join(secrets.choice(string.ascii_letters + string.digits + string.punctuation) for passwd in range(pass_length))
+            print(password)
+
+
+        
+
+password_generator()
